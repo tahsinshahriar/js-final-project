0e99d1a5e62fa3689c1983705b502d65ec5878f7
Sheikh Tahsin Shahriar <shafin_008@hotmail.com>
2021-01-24 21:05:44 +0600

    deposit button event added

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ loginButton.addEventListener("click",function(){
     document.getElementById("login-area").style.display = "none";
     document.getElementById("transaction-area").style.display = "block";
 })
+
+//deposit button event
+const depositBtn = document.getElementById("deposit-btn");
+depositBtn.addEventListener("click", function(){
+    const depositAmmount = parseFloat(document.getElementById("deposit-amt").value);
+    const currDeposit = parseFloat(document.getElementById("curr-deposit").innerHTML);
+    const totalBalance = parseFloat(document.getElementById("total-balance").innerHTML);
+    document.getElementById("curr-deposit").innerHTML = currDeposit + depositAmmount;
+    document.getElementById("total-balance").innerHTML = totalBalance + depositAmmount;
+    document.getElementById("deposit-amt").value = "";
+})
